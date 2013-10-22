c395d7d5b8c4840a219807909a52601d2b8835b3
Chris Oei <code@oei.io>
2013-10-22 00:04:27 +0000

    Centralize static config

--- build.bash
+++ build.bash
@@ -2,26 +2,28 @@
 
 git submodule update --init
 
+export STATIC_CONFIG="--enable-static --disable-shared --extra-ldflags=-static"
+
 for d in fdk-aac ogg vorbis flac opus
 do
   pushd "$d" 
   [ -x ./autogen.sh ] && ./autogen.sh
-  ./configure --enable-static --disable-shared
+  ./configure $STATIC_CONFIG
   make && sudo make install
   popd
 done
 
 pushd x264
-./configure --enable-static --disable-shared --disable-cli --disable-opencl
+./configure $STATIC_CONFIG --disable-cli --disable-opencl
 make && sudo make install
 popd
 
 pushd libvpx
-./configure --enable-static --disable-shared --enable-vp9
+./configure $STATIC_CONFIG --enable-vp9
 make && sudo make install
 popd
 
 pushd ffmpeg
-./configure --disable-shared --extra-ldflags=-static --enable-{gpl,nonfree,static,lib{fdk-aac,mp3lame,opus,vorbis,vpx,x264}}
+./configure $STATIC_CONFIG --enable-{gpl,nonfree,static,lib{fdk-aac,mp3lame,opus,vorbis,vpx,x264}}
 make && sudo make install
 popd
