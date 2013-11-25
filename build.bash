46e22f204f1ddb12f9636ca29b9abad44160b28b
Chris Oei <code@oei.io>
2013-11-25 12:04:53 -0800

    OS X changes

--- build.bash
+++ build.bash
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-git submodule update --init
+#git submodule update --init
 
-export STATIC_CONFIG="--enable-static --disable-shared --extra-ldflags=-static"
+#export STATIC_CONFIG="--enable-static --disable-shared --extra-ldflags=-static"
+export STATIC_CONFIG="--enable-static --disable-shared --prefix=/opt/media-tools"
 
 for d in fdk-aac ogg vorbis flac opus
 do
   pushd "$d" 
   [ -x ./autogen.sh ] && ./autogen.sh
   ./configure $STATIC_CONFIG
-  make && sudo make install
+  make && make install
   popd
 done
 
 pushd x264
 ./configure $STATIC_CONFIG --disable-cli --disable-opencl
-make && sudo make install
+make && make install
 popd
 
 pushd libvpx
 ./configure $STATIC_CONFIG --enable-vp9
-make && sudo make install
+make && make install
 popd
 
 pushd ffmpeg
 ./configure $STATIC_CONFIG --enable-{gpl,nonfree,static,lib{fdk-aac,mp3lame,opus,vorbis,vpx,x264}}
-make && sudo make install
+make && make install
 popd
