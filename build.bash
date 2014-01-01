6384e0824c2a2eaea8dc581258323d3447a295b3
Chris Oei <code@oei.io>
2014-01-01 00:38:29 +0000

    Remove sudo

--- build.bash
+++ build.bash
@@ -9,21 +9,21 @@ do
   pushd "$d" 
   [ -x ./autogen.sh ] && ./autogen.sh
   ./configure $STATIC_CONFIG
-  make && sudo make install
+  make && make install
   popd
 done
 
 pushd x264
 ./configure $STATIC_CONFIG --disable-cli --disable-opencl
-make && sudo make install
+make && make install
 popd
 
 pushd libvpx
 ./configure $STATIC_CONFIG --enable-vp9
-make && sudo make install
+make && make install
 popd
 
 pushd ffmpeg
 ./configure $STATIC_CONFIG --enable-{gpl,nonfree,static,lib{fdk-aac,mp3lame,opus,vorbis,vpx,x264}}
-make && sudo make install
+make && make install
 popd
