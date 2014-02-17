ba950aecbe4757419fc4699fa7aa6a934bbb65ad
Chris Oei <code@oei.io>
2014-02-17 15:48:07 -0800

    Make ffmpeg and key codecs use master

--- build.bash
+++ build.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -e /opt/media-tools ]
+then
+  sudo mkdir /opt/media-tools && sudo chown c /opt/media-tools
+fi
+
 git submodule update --init
 
 export STATIC_CONFIG="--enable-static --disable-shared --prefix=/opt/media-tools"
@@ -14,16 +19,19 @@ do
 done
 
 pushd x264
+git checkout master && git pull
 ./configure $STATIC_CONFIG --disable-cli --disable-opencl
 make && make install
 popd
 
 pushd libvpx
+git checkout master && git pull
 ./configure $STATIC_CONFIG --enable-vp9
 make && make install
 popd
 
 pushd ffmpeg
-./configure $STATIC_CONFIG --enable-{gpl,nonfree,static,lib{fdk-aac,mp3lame,opus,vorbis,vpx,x264}} --extra-cflags=-I/opt/media-tools/include --extra-ldflags=-L/opt/media-tools/lib
+git checkout master && git pull
+./configure $STATIC_CONFIG --enable-{gpl,nonfree,static,lib{fdk-aac,mp3lame,opus,vorbis,vpx,x264}} --extra-cflags=-I/opt/media-tools/include --extra-ldflags=-L/opt/media-tools/lib --disable-avcodec
 make && make install
 popd
